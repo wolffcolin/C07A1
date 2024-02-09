1830e1737a2c48595a732e10ded03c98c78c5019
wolffcolin <cwcnsa@gmail.com>
2024-02-09 15:38:30 -0700

    fixing mistakenn push

--- src/tests/TestInteractions.java
+++ src/tests/TestInteractions.java
@@ -4,11 +4,29 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-class TestInteractions {
+import src.gameEngine.Drawable;
+import src.gameEngine.GameEngine;
+import src.gameEngine.InteractionResult;
+import src.levelPieces.Skeleton;
 
+class TestInteractions {
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testSkeleton() {
+		Drawable[] gameBoard = new Drawable[GameEngine.BOARD_SIZE];
+		Skeleton skeleton = new Skeleton('S', "skeleton", 10);
+		gameBoard[10] = skeleton;
+		
+		assertEquals(InteractionResult.HIT, skeleton.interact(gameBoard, 10));
+		
+		for (int i = 0; i < 10; i++) {
+			assertEquals(InteractionResult.HIT, skeleton.interact(gameBoard, i));
+		}
+		
+		for (int i = 11; i < GameEngine.BOARD_SIZE; i++) {
+			assertEquals(InteractionResult.NONE, skeleton.interact(gameBoard, i));
+		}
+		
 	}
 
 }
--- src/tests/TestMovingPieces.java
+++ src/tests/TestMovingPieces.java
@@ -16,22 +16,5 @@ class TestMovingPieces {
 		fail("Not yet implemented");
 	}
 	
-	@Test
-	void testSkeletonMovement() {
-		Drawable[] gameBoard = new Drawable[GameEngine.BOARD_SIZE];
-		Skeleton skeleton = new Skeleton('S', "skeleton", 10);
-		gameBoard[10] = skeleton;
-		
-		assertEquals(InteractionResult.HIT, skeleton.interact(gameBoard, 10));
-		
-		for (int i = 0; i < 10; i++) {
-			assertEquals(InteractionResult.HIT, skeleton.interact(gameBoard, i));
-		}
-		
-		for (int i = 11; i < GameEngine.BOARD_SIZE; i++) {
-			assertEquals(InteractionResult.NONE, skeleton.interact(gameBoard, i));
-		}
-		
-	}
 
 }
