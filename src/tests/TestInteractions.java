275d838be223c4696edb1abed44b8d0530c41f91
wolffcolin <cwcnsa@gmail.com>
2024-02-09 15:30:56 -0700

    testing framework

---
+++ src/tests/TestInteractions.java
@@ -0,0 +1,14 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestInteractions {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
