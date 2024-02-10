88d6c679c87e11d6dba8293c335b1b82000249e6
mmcgrath2 <mmcgrath1@mines.edu>
2024-02-09 18:12:01 -0700

    Added the rest of the interaction tests

--- src/tests/TestInteractions.java
+++ src/tests/TestInteractions.java
@@ -17,7 +17,10 @@ class TestInteractions {
 		Skeleton skeleton = new Skeleton('S', "skeleton", 10);
 		gameBoard[10] = skeleton;
 		
-		assertEquals(InteractionResult.HIT, skeleton.interact(gameBoard, 10));
+		for(int i = 8; i < 13; i++) {
+			assertEquals(InteractionResult.HIT, skeleton.interact(gameBoard, i));
+		}
+		
 		
 		for (int i = 0; i < 10; i++) {
 			assertEquals(InteractionResult.HIT, skeleton.interact(gameBoard, i));
@@ -28,5 +31,75 @@ class TestInteractions {
 		}
 		
 	}
+	@Test
+	void testBat() {
+		Drawable[] gameBoard = new Drawable[GameEngine.BOARD_SIZE];
+		Bat bat = new Bat('B', "bat", 10);
+		gameBoard[10] = bat;
+		
+		assertEquals(InteractionResult.HIT, bat.interact(gameBoard, 10);
+		
+		for (int i = 0; i < 10; i++) {
+			assertEquals(InteractionResult.NONE, bat.interact(gameBoard, i);
+		}
+		
+		for (int i = 11; i < GameEngine.Board_Size; i++) {
+			assertEquals(InteractionResult.NONE, bat.interact(gameBoard, i);
+		}
+	}
+	
+	@Test
+	void testFairy() {
+		Drawable[] gameBoard = new Drawable[GameEngine.BOARD_SIZE];
+		Fairy fairy = new Fairy('F', "fairy", 10);
+		gameBoard[10] = fairy;
+		
+		assertEquals(InteractionResult.ADD_POINT, fairy.interact(gameBoard, 10);
+		
+		for (int i = 0; i < 10; i++) {
+			assertEquals(InteractionResult.NONE, fairy.interact(gameBoard, i);
+		}
+		
+		for (int i = 11; i < GameEngine.Board_Size; i++) {
+			assertEquals(InteractionResult.NONE, fairy.interact(gameBoard, i);
+		}
+	}
+	
+	@Test
+	void testGuard() {
+		Drawable[] gameBoard = new Drawable[GameEngine.BOARD_SIZE];
+		Guard guard = new Guard('G', "guard", 10);
+		gameBoard[10] = guard;
+		
+		assertEquals(InteractionResult.KILL, guard.interact(gameBoard, 10);
+		
+		for (int i = 0; i < 10; i++) {
+			assertEquals(InteractionResult.NONE, guard.interact(gameBoard, 10);
+		}
+		
+		for (int i = 11; i < GameEngine.Board_Size; i++) {
+			assertEquals(InteractionResult.NONE, guard.interact(gameBoard, 10);
+		}
+	}
+	
+	@Test
+	void testMage() {
+		Drawable[] gameBoard = new Drawable[GameEngine.BOARD_SIZE];
+		Mage mage = new Mage('M', "Mage", 10);
+		gameBoard[10] = mage;
+		
+		assertEquals(InteractionResult.HIT, mage.interact(gameBoard, 9);
+		assertEquals(InteractionResult.HIT, mage.interact(gameBoard, 11);
+		assertEquals(InteractionResult.ADVANCE, mage.interact(gameBoard, 10);
+		
+		for (int i = 0; i < 9; i++) {
+			assertEquals(InteractionResult.NONE, mage.interact(gameBoard, i);
+		}
+		
+		for (int i = 12; i < GameEngine.Board_Size; i++) {
+			assertEquals(InteractionResult.NONE, mage.interact(gameBoard, i);
+		}
+	}
+	
 
 }
